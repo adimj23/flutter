f202f891619816124a5abf0f54134622972bbc71
ansrinivasan <54161806+ansrinivasan@users.noreply.github.com>
2021-01-12 21:19:37 -0600

    Created project from command line

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -10,33 +10,57 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.green),
-      home: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {
-            print("FAB Pressed");
-          },
+      home: MyHomePage(title: "Flutter Demo Home Page"),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "You've pushed this button this many times:",
+            ),
+            Text(
+              "$_counter",
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            IconButton(
+              icon: Icon(Icons.star),
+            ),
+          ],
         ),
-        appBar: AppBar(
-          title: Text("Flutter Demo Home Page"),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                "You've pushed this button this many times:",
-              ),
-              Text(
-                "0",
-                style: Theme.of(context).textTheme.headline4,
-              ),
-              IconButton(
-                icon: Icon(Icons.star),
-              ),
-            ],
-          ),
-        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: "Increment",
+        child: Icon(Icons.add),
       ),
     );
   }
