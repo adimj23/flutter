4fcf591efde87680668b3b3eff657539b0a3c807
ansrinivasan <54161806+ansrinivasan@users.noreply.github.com>
2021-01-12 20:26:33 -0600

    Created various widgets to replicate main app

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -9,9 +9,33 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.green),
       home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            print("FAB Pressed");
+          },
+        ),
+        appBar: AppBar(
+          title: Text("Flutter Demo Home Page"),
+        ),
         body: Center(
-          child: Text("Hello Flutter!"),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "You've pushed this button this many times:",
+              ),
+              Text(
+                "0",
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              IconButton(
+                icon: Icon(Icons.star),
+              ),
+            ],
+          ),
         ),
       ),
     );
