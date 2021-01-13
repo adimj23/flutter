012d4c69a4f1b2b01f9502794827ef9ca6323e8b
ansrinivasan <54161806+ansrinivasan@users.noreply.github.com>
2021-01-13 11:24:03 -0600

    Container widget

--- more_widgets/lib/main.dart
+++ more_widgets/lib/main.dart
@@ -1,1 +1,40 @@
-void main() {}
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My home page"),
+        ),
+        body: Center(
+          child: Container(
+            color: Colors.yellow,
+            padding: EdgeInsets.all(12),
+            margin: EdgeInsets.all(24),
+            alignment: Alignment.center,
+            constraints: BoxConstraints.expand(height: 300),
+            decoration: BoxDecoration(
+              color: Colors.redAccent,
+              border: Border.all(
+                width: 4,
+                color: Colors.blue,
+              ),
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: Text(
+              "This is a text widget. This widget is inside a Container widget.",
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
