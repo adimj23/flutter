8845fc95e7a2192e7b6c4dd5e0667924bedae166
ansrinivasan <54161806+ansrinivasan@users.noreply.github.com>
2021-01-14 22:39:00 -0600

    Added pages

--- my_bookmarks/lib/widget/bookmark_list_item_widget.dart
+++ my_bookmarks/lib/widget/bookmark_list_item_widget.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:my_bookmarks/model/bookmark.dart';
+import 'package:my_bookmarks/page/add_bookmark_page.dart';
+import 'package:my_bookmarks/page/view_bookmark_page.dart';
+import 'package:my_bookmarks/util/navigation_util.dart';
 
 class BookmarkListItemWidget extends StatelessWidget {
   Bookmark bookmark;
@@ -11,12 +14,16 @@ class BookmarkListItemWidget extends StatelessWidget {
     // TODO: implement build
     return Padding(
       padding: const EdgeInsets.all(12),
-      child: Column(
-        children: [
-          Text(bookmark.title, style: Theme.of(context).textTheme.headline6),
-          SizedBox(height: 6),
-          Text(bookmark.link, style: Theme.of(context).textTheme.subtitle1),
-        ],
+      child: InkWell(
+        onTap: () => navigateToViewBookmarkPage(context),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(bookmark.title, style: Theme.of(context).textTheme.headline6),
+            SizedBox(height: 6),
+            Text(bookmark.link, style: Theme.of(context).textTheme.subtitle1),
+          ],
+        ),
       ),
     );
   }
